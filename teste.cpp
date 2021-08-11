282b245342af409d187184effae469866212b84a
Patrick Leal <80593781+Letrick@users.noreply.github.com>
2021-08-11 20:06:41 -0300

    Add files via upload

---
+++ teste.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+
+
+int main (){
+	
+int numero;
+
+std::cout << "digite um numero: ";
+std::cin  >>numero;
+
+
+ if (numero > 20){
+ 
+ 	std::cout << "O numero e: " << numero;
+}
+ else{
+ 
+ 	std::cout << "Tente novamente!";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+}
+
+
+
