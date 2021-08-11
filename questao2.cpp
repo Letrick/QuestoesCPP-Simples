af853e23494127259f7100193d7cb9d65003421d
Patrick Leal <80593781+Letrick@users.noreply.github.com>
2021-08-11 20:34:18 -0300

    Add files via upload

---
+++ questao2.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+
+int main(){
+	
+	int numero1, numero2, resultado;
+	
+	
+	std::cout << "Digite o primeiro numero para somar: ";
+	std::cin  >> numero1;
+	
+	std::cout << "Digite o segundo numero para somar: ";
+	std::cin  >> numero2;
+	
+	resultado = numero1 + numero2;
+	if(resultado > 10){
+	std::cout << "Resultado da soma: " << resultado;
+	}
+	
+	else{
+		
+		std::cout << "errou ";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
