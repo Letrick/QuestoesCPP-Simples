7770346a8d5810832aebdc6e42328fb3b19df3d5
Patrick Leal <80593781+Letrick@users.noreply.github.com>
2021-08-11 20:41:57 -0300

    Add files via upload

---
+++ questao 3.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+int main () {
+	
+	int numero;
+	
+	std::cout << "Digite um numero e saberemos se e PAR ou IMPAR: ";
+	std::cin  >> numero;
+	
+	
+	if(numero % 2 == 0){
+	std::cout << "Par";
+		
+		
+	}
+	else{
+	std::cout << "Impar";
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
