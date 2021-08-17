d34031f4ac74ecd0438f04eb24f243e1fa8b99dc
Patrick Leal <80593781+Letrick@users.noreply.github.com>
2021-08-16 22:06:11 -0300

    Add files via upload

---
+++ exercicio1-nivel2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+
+float celsius, Fahrenheit, kelvin;
+
+	std::cout << "Digite graus em celsius: ";
+	std::cin  >> celsius;
+
+	kelvin = celsius + 273.15;
+
+
+
+	
+	std::cout << "Celsius convertido em Kelvin: " << kelvin;
+	
+
+	
+
+}
