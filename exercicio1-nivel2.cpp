55886628847b93e5b621193287092e9a477d6ecb
Patrick Leal <80593781+Letrick@users.noreply.github.com>
2021-08-16 21:42:12 -0300

    Add files via upload

---
+++ exercicio1-nivel2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+int main(){
+
+float velMedia, celsius, calcFahrenheit, QtdeLitros;
+
+	std::cout << "Digite graus em Celsius: ";
+	std::cin  >> celsius;
+
+	calcFahrenheit = ( 9 * celsius + 160 ) / 5;
+
+
+
+
+	std::cout << "\n";
+	
+	std::cout << "Celsius convertido em Fahrenheit: " << calcFahrenheit;
+	
+
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
